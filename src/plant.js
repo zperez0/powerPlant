be504ad5000a65cbc044022f611d0f9915661daa
Yazmin Perez <zoeperez333@gmail.com>
2022-07-20 11:16:41 -0700

    [WIP] fix spacing, add notes & call changeState()

---
+++ src/plant.js
@@ -0,0 +1,32 @@
+// storing the state
+const storeState = (initialState) => {
+  let currentState = initialState;
+  return (stateChangeFunction = state => state) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = { ...newState };
+    return newState;
+  };
+};
+
+// to store & invoke storeState()
+export const stateControl = storeState();
+
+// function factory
+export const changeState = (state) => {
+  return (value) => {
+    return (state) => ({
+      ...state,
+      [prop]: (state[prop] || 0) + value,
+    });
+  };
+};
+
+// create functions using the function factory & call changeState()
+const feed = changeState("soil")(1);
+const blueFood = changeState("soil")(5);
+
+const hydrate = changeState("water")(1);
+const superWater = changeState("water")(5);
+
+const light = changeState("light")(1);
+const superLight = changeState("light")(5);
