3e23dd5591be37da04477b2c46e049f10ba26c61
Yazmin Perez <zoeperez333@gmail.com>
2022-07-20 11:15:07 -0700

    [WIP] add imports & jquery

--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import {stateControl, changeState} from './src/plant.js'
 
-const changeState = (state) => {
-  return (value) => {
-    return (state) => ({
-      ...state,
-      [prop]: (state[prop] || 0) + value
-    })
-  }
-}
+$(document).ready(function () {
+  const newState = stateControl(blueFood);
+  $("soil-value").text(`Soil: ${newState.soil}`);
 
-
-// storing the state
-const storeState = () => {
-  let currentState = {};
-  return (stateChangeFunction) => {
-    const newState = stateChangeFunction(currentState);
-    currentState = {...newState};
-    return newState;
-  }
-}
-
-const stateControl = storeState();
+  // for demostration purposes only (does not do anything in this app)
+  //to see the current state
+  $("#show-state").click(function () {
+    // call stateControl() without agruments to see current state
+    const currentState = stateControl();
+    $("#soil-value").text(`Soil: ${currentState.soil}`);
+  });
+});
